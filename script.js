796aca3781c062b0fc0878db3d5e8a9c6bb28fd5
floroldos <florencia.roldos@correo.ucu.edu.uy>
2023-08-14 15:26:05 -0300

    cositas

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const buttonA = document.querySelector("#button_A");
-const headingA = document.querySelector("#heading_A");
 
-buttonA.onclick = () => {
+hacerAlerta : buttonA.onclick = () => {
   const name = prompt("Ingrese el nombre de la tarjeta");
-  alert(`Taejeta ${name} creada satisfactoriamente`);
-};
+  const description = prompt("Ingrese la descripción de la tarjeta");
+
+  alert(`Taejeta ${name} creada satisfactoriamente.`);
+  crearTarjeta(name, description);
+}
+function crearTarjeta (nombreTarjeta, descripcionTarjeta){
+  let tarjeta = [" "]
+  card += `
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title"> ${nombreTarjeta} </h5>
+    <p class="card-text">${descripcionTarjeta}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`
+const panel = document.getElementById("panel");
+panel.innerHTML = card;
+}
+
+const pannel = document.getElementById("panel");
+pannel.innerHTML = card;
