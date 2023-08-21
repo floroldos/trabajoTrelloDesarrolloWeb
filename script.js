828c7464e5a8aa916ac1ede19a007db5881fd3ce
floroldos <florencia.roldos@correo.ucu.edu.uy>
2023-08-21 08:21:44 -0300

    arreglitos

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ hacerColumna : botonColumna.onclick = () => {
 
 function crearColumna (nombreColumna){
   let columna = `
-      <div class="row justify-content-center col-4 contenedor" ondrop="drop(event)" ondragover="allowDrop(event)">
+      <div class="row justify-content-center col-3 contenedor" ondrop="drop(event)" ondragover="allowDrop(event)">
         <div class="tituloColumnas">
           <p>
             ${nombreColumna}
@@ -44,7 +44,7 @@ function crearColumna (nombreColumna){
       </div>
   `;
   const nuevaColumna = document.createElement("div"); // Crear un nuevo elemento div para la columna
-  nuevaColumna.className = "col-4 columna";
+  nuevaColumna.className = "col-3 columna";
   nuevaColumna.innerHTML = columna; // Asignar el contenido HTML al nuevo div
 
   const contenedor = document.getElementById("panel");
@@ -99,7 +99,6 @@ function botonCrearTarjeta(boton){
 function crearTarjeta (boton, name, description){
   let tarjeta = `
     <div class="card" draggable="true">
-      <img class="card-img-top" src="img/Imagen1.svg">
       <div class="card-body">
         <h4 class="card-title"> ${name} </h4>
         <hr width="100%">
@@ -107,7 +106,7 @@ function crearTarjeta (boton, name, description){
       </div>
       <div class="botones">
         <button onclick ="alertaBorrarT(this)" type="button" aria-label="Trash" margin-left=5px class="botonCard">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
           </svg>    
         </button>
