6ef6da404a48fcac1d67d9f11683d8f8393e7d98
floroldos <florencia.roldos@correo.ucu.edu.uy>
2023-08-20 21:04:50 -0300

    algunos arreglos de las alertas

--- script.js
+++ script.js
@@ -18,11 +18,6 @@ function crearColumna (nombreColumna){
         </p>
       </div>
       <div class="botones">
-        <button  onclick = "alertaBorrarC(this)" type="button" id="delete" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-          </svg>
-        </button>
         <button  onclick = "botonCrearTarjeta(this)" type="button" id="button_A" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -34,6 +29,11 @@ function crearColumna (nombreColumna){
             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
           </svg>
         </button>
+        <button  onclick = "alertaBorrarC(this)" type="button" id="delete" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+          </svg>
+        </button>
       </div>
       <div class="linea">
         <hr/>
@@ -42,12 +42,12 @@ function crearColumna (nombreColumna){
       </div>
     </div>
   `;
-  const nuevaColumna = document.createElement("div"); // Crear un nuevo elemento div para la tarjeta
+  const nuevaColumna = document.createElement("div"); // Crear un nuevo elemento div para la columna
   nuevaColumna.className = "col-4 columna";
-  nuevaColumna.innerHTML = columna; // Asignar el contenido HTML de la tarjeta al nuevo div
+  nuevaColumna.innerHTML = columna; // Asignar el contenido HTML al nuevo div
 
   const contenedor = document.getElementById("panel");
-  contenedor.appendChild(nuevaColumna); // Agregar la nueva tarjeta al panel
+  contenedor.appendChild(nuevaColumna); // Agregar la nueva columna
 }
 
 function borrarTarjeta(botonB) {
@@ -63,8 +63,10 @@ function alertaBorrarC(boton){
   if(confirm("¿Estas seguro que quieres eliminar el elemento?")){
     texto = "Se eliminó el elemento."; 
     borrarColumna(boton);
+    alert(texto);
   } else {
     texto = "No se eliminó ningun elemento.";
+    alert(texto);
   }
 }
 
@@ -73,8 +75,10 @@ function alertaBorrarT(boton){
   if(confirm("¿Estas seguro que quieres eliminar el elemento?")){
     texto = "Se eliminó el elemento."; 
     borrarTarjeta(boton);
+    alert(texto);
   } else {
     texto = "No se eliminó ningun elemento.";
+    alert(texto);
   }
 }
 
