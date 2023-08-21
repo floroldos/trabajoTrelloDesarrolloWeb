8a6522505122e562d0114302324b9e4d9738d9c8
Florencia Roldos <110829678+floroldos@users.noreply.github.com>
2023-08-21 18:02:40 -0300

    arreglos para merge

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
+/// Global
+
+var tarjId = 0;
+
+class tarea{
+  constructor(nombre, descripcion) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+  }
+}
 
 /// Funciones Principales ///
 
 // Crar Columna //
 function crearColumna (nombreColumna){
   let columna = `
-    <div class="row justify-content-center col-3 contenedor" ondrop="drop(event)" ondragover="allowDrop(event)">
-      <div class="tituloColumnas">
-        <p>
-          ${nombreColumna}
-        </p>
+      <div class="row justify-content-center col-3 contenedor" ondrop="drop(event)" ondragover="allowDrop(event)">
+        <div class="tituloColumnas">
+          <p>
+            ${nombreColumna}
+          </p>
+        </div>
+        <div class="botones">
+          <button  onclick = "botonCrearTarjeta(this)" type="button" id="button_A" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
+          </button>
+          <button onclick = "contraerDescontraer(this)" type="button" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+            </svg>
+          </button>
+          <button  onclick = "alertaBorrarC(this)" type="button" id="delete" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+            </svg>
+          </button>
+        </div>
+        <div class="linea">
+          <hr/>
+        </div>
+        <div class = "contenido" style="display: block;"  ondrop="drop(event)" ondragover="allowDrop(event)">
+        </div>
       </div>
-      <div class="botones">
-        <button  onclick = "botonCrearTarjeta(this)" type="button" id="button_A" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-          </svg>
-        </button>
-        <button onclick = "botonContraerDecontraer(this)" type="button" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
-          </svg>
-        </button>
-        <button  onclick = "alertaBorrarC(this)" type="button" id="delete" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-          </svg>
-        </button>
-      </div>
-      <div class = "contenido" style="display: block;"  ondrop="drop(event)" ondragover="allowDrop(event)">
-      </div>
-    </div>
   `;
-  const nuevaColumna = document.createElement("div"); // Crear un nuevo elemento div para la tarjeta
+  const nuevaColumna = document.createElement("div"); // Crear un nuevo elemento div para la columna
   nuevaColumna.className = "col-3 columna";
-  nuevaColumna.innerHTML = columna; // Asignar el contenido HTML de la tarjeta al nuevo div
+  nuevaColumna.innerHTML = columna; // Asignar el contenido HTML al nuevo div
 
   const contenedor = document.getElementById("panel");
-  const boton = document.getElementById("button")
-  contenedor.insertBefore(nuevaColumna, boton); // Agregar la nueva columna
+  contenedor.appendChild(nuevaColumna); // Agregar la nueva columna
 }
 
 // Crear Tarjeta //
@@ -46,12 +58,12 @@ function crearTarjeta (boton, name, description){
   let tarjeta = `
     <div class="card" draggable="true">
       <div class="card-body">
-        <h4 class="card-title"> ${name} </h4>
+        <h4 class="card-title" id = ${tarjId}> ${name} </h4>
         <hr width="100%">
         <p class="card-text"> ${description} </p>
       </div>
       <div class="botones">
-        <button onclick ="alertaBorrarT(this)" type="button" aria-label="Trash" margin-left=5px class="botonCard">
+        <button onclick ="alertaBorrarT(this)" type="button" id="borrarTarea" aria-label="Trash" margin-left=5px class="botonCard">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
           </svg>    
@@ -67,6 +79,52 @@ function crearTarjeta (boton, name, description){
   const contenido = contenedor.querySelector('.contenido');
 
   contenido.appendChild(nuevaTarjeta);
+
+  let tarea1 = new tarea(name, description);
+
+  let jstring = JSON.stringify(tarea1);
+  localStorage.setItem(tarjId, jstring);
+  tarjId ++;
+}
+
+// Para cargar los datos del localstorage //
+
+function cargarJson(){
+  const contenedor = document.getElementById("panel");
+
+  console.log(localStorage.length);
+
+  for (let i = 0; i <= localStorage.length; i++){
+    tarjId = i;
+    let obj = JSON.parse(localStorage.getItem(i));
+
+    console.log(obj.nombre);
+
+    let tarjeta = `
+    <div class="card" draggable="true" id = "tarea">
+      <div class="card-body">
+        <h4 class="card-title"> ${obj.nombre} </h4>
+        <hr width="100%">
+        <p class="card-text"> ${obj.descripcion} </p>
+      </div>
+      <div class="botones">
+        <button onclick ="alertaBorrarT(this)" type="button"  id="borrarTarea" aria-label="Trash" margin-left=5px class="botonCard">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+          </svg>    
+        </button>
+      </div>
+    </div>
+  `;
+
+  const nuevaTarjeta = document.createElement("div"); // Crear un nuevo elemento div para la tarjeta
+  nuevaTarjeta.innerHTML = tarjeta; // Asignar el contenido HTML de la tarjeta al nuevo div
+
+  const contenido = contenedor.querySelector('.contenido');
+
+  contenido.appendChild(nuevaTarjeta);
+
+  }
 }
 
 // Borrar Columna //
@@ -75,8 +133,16 @@ function borrarColumna(botonBorrar){
 }
 
 // Borrar Tarjeta //
-function borrarTarjeta(botonB) {
-  botonB.parentNode.parentNode.remove();
+function borrarTarjeta(boton) {
+  boton.parentNode.parentNode.remove();
+  for(let i = 0; i <= localStorage.length; i++){
+    if(localStorage.key(i) == boton.getElementById(id)){
+      let clave = localStorage.key(i);
+      break;
+    }
+  }
+  console.log(clave);
+  localStorage.removeItem(clave);
 }
 
 
@@ -111,7 +177,7 @@ function contraerDescontraer(boton){
 
 /// Botones ///
 
-// Crar Columna //
+// Crear Columna //
 function botonCrearColumna(){
   const nombre = prompt("Ingrese el nombre de la columna")
   if(nombre !== null && nombre.trim() !== ""){
@@ -138,8 +204,10 @@ function alertaBorrarC(boton){
   if(confirm("¿Estas seguro que quieres eliminar el elemento?")){
     texto = "Se eliminó el elemento."; 
     borrarColumna(boton);
+    alert(texto);
   } else {
     texto = "No se eliminó ningun elemento.";
+    alert(texto);
   }
 }
 
@@ -147,10 +215,10 @@ function alertaBorrarC(boton){
 function alertaBorrarT(boton){
   let texto;
   if(confirm("¿Estas seguro que quieres eliminar el elemento?")){
-    texto = "Se eliminó el elemento."; 
     borrarTarjeta(boton);
   } else {
     texto = "No se eliminó ningun elemento.";
+    alert(texto);
   }
 }
 
@@ -159,6 +227,42 @@ function botonContraerDecontraer(boton){
   contraerDescontraer(boton)
 }
 
+// DRAG AND DROP //
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+// Cambiar fondo //
+
+function cambiaFondo(){
+  const button = document.getElementById('cambiarEstilo');
+    let estiloActual = 0;
+
+    button.addEventListener('click', () => {
+      estiloActual = (estiloActual + 1) % 2;
+
+      if (estiloActual === 1) {
+        document.body.style.backgroundColor = '#f0f0f0';
+        document.body.style.color = '#333';
+        button.style.backgroundColor = '#ff6600';
+      } else {
+        document.body.style.backgroundColor = '#fff';
+        document.body.style.color = '#000';
+        button.style.backgroundColor = '#007bff';
+      }
+    });
+}
 
 /// Main ///
 
