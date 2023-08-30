708f02068be66103b84c45b8f7cab9cf6517dfaa
floroldos <florencia.roldos@correo.ucu.edu.uy>
2023-08-30 10:51:53 -0300

    arreglos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /// Global
 var tarjId = 0;
+var colId = 0;
 
 class tarea {
   constructor(nombre, descripcion, listaBullets) {
@@ -32,79 +33,89 @@ function craerBoard (nombreBoard){
 }
 
 // Crear Columna //
-function crearColumna(nombreColumna) {
-    let columna = document.createElement("div");
-    columna.className = "col-3 columna";
-    columna.innerHTML = `
-        <div class="row justify-content-center col-3 contenedor">
-            <div class="tituloColumnas">
-                <textarea id="nombreColumna" class="list-header-name mod-list-name js-list-name-input" aria-label="${nombreColumna}" spellcheck="false" dir="auto" maxlength="80" data-autosize="true">${nombreColumna}</textarea>
-                <button onclick="toggleColumn(this)" type="button" margin-left="5px" class="botonColumnStyle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
-                    </svg>
-                </button>
-            </div>
-            <div class="input-group w-auto">
-                <input type="text" id="inputName" class="form-control" placeholder="Nueva tarjeta..." aria-label="Example input" ria-describedby="nameCard"/>
-                <button class="btn btn-primary" type="button" onclick="crearTarjeta(this)">+</button>
-            </div>
-            <div class="contenido">
-                <!-- Aca se agregan tarjetas -->
-            </div>
-            <div class="botones">
-                <button onclick="borrarColumna(this)" type="button" margin-left="5px" class="botonColumnStyle btn btn-outline-light rounded-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-                    </svg>
-                </button>
-            </div>
-        </div>
-    `;
+function crearColumna (){
+  let inputName = document.getElementById("panel");
+  let nameColumn = document.getElementById("inputColumnName").value;
 
-    let contenedor = document.getElementById("panel");
-    let boton = document.getElementById("button");
-    contenedor.insertBefore(columna, boton);
+  if (nameColumn){
+    let = `column-${colId}`;
+    let column = document.createElement("div");
+    column.className = "col-3 column";
+    column.id = columnId;
+    column.innerHTML = `
+    <div class="row justify-content-center col-3 contenedor" id = "columna">
+      <div class="tituloColumnas">
+        <input type="text" maxlenght="10" class="${nameColumn}" style="color: white;" contentEditable="true" data-aut>${nameColumn}</h4>
+        <button onclick = "botonContraerDecontraer(this)" type="button" margin-left=5px class="botonColumnStyle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+          </svg>
+        </button>
+      </div>
+      <div class="contenidoCompleto" id="contenidoCompleto">        
+        <div class="input-group w-auto">
+          <input type="text" id="inputCardName" class="form-control enter-as-button" placeholder="Add Card" aria-label="Example input" ria-describedby="nameCard"/>
+          <button class="btn btn-primary enter-button" type="button" onclick="crearTarjeta(this)" data-mdb-ripple-color="dark">
+              +
+          </button>
+        </div>
+        <div class = "contenido" id="contenido" ondrop="drop(event)" ondragover="allowDrop(event)">
+        </div>
+        <div class="botones">
+          <button  onclick = "setIdDeleteColumn(${columnId})" type="button" id="delete" margin-left=5px class="botonColumnStyle btn btn-outline-light rounded-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  panel.insertBefore(column, button);
+  colId ++;
+  }
 }
 
 function crearTarjeta(button) {
-    let inputName = button.parentNode.querySelector("#inputName");
+    let inputName = button.parentNode.querySelector("#inputCardName");
     let name = inputName.value;
-    if (name.trim() !== "") {
+    if (name) {
         let columna = button.closest(".contenedor");
         let contenido = columna.querySelector(".contenido");
+        console.log(contenido);
         let cardId = `card-${tarjId}`;
         let card = document.createElement("div");
         card.className = "card";
         card.id = cardId;
         card.innerHTML = `
             <div class="card-body">
-                <div class="titulo">
-                    <h4 class="${cardId}" style="color: black;">${name}</h4>
-                    <div class="dropdown">
-                      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownTarjeta" data-bs-toggle="dropdown" aria-expanded="false">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-                          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                        </svg>
-                      </button>
-                      <ul class="dropdown-menu" aria-labelledby="dropdownTarjeta">
-                          <li>
-                            <button id = "dropdownEditar" class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#editarTarjeta" onclick="editarTarjeta('${cardId}')">
-                              Editar
-                            </button>
-                          </li>
-                          <li>
-                            <button id = "dropdownBorrar" class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#eliminarTarjeta" onclick="setIdToDeleteCard('${cardId}')">
-                              Borrar
-                            </button>
-                          </li>
-                          <li>
-                            <button id = "dropdownDuplicar" class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#duplicarTarjeta">
-                              Duplicar
-                            </button>
-                          </li>
-                        </ul>
-                    </div>
+                <div class="tituloCard">
+                  <h4 class="${cardId}" style="color: black;" contentEditable="true">${name}</h4>
+                  <div class="dropdown">
+                    <button class="btn btn-primary" prtype="button" id="dropdownTarjeta" data-bs-toggle="dropdown" aria-expanded="false">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                      </svg>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownTarjeta">
+                        <li>
+                          <button id = "dropdownEditar" class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#editarTarjeta" onclick="editarTarjeta('${cardId}')">
+                            Editar
+                          </button>
+                        </li>
+                        <li>
+                          <button id = "dropdownBorrar" class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#eliminarTarjeta" onclick="setIdToDeleteCard('${cardId}')">
+                            Borrar
+                          </button>
+                        </li>
+                        <li>
+                          <button id = "dropdownDuplicar" class="btn btn-secondary" type="button" data-bs-toggle="modal" data-bs-target="#duplicarTarjeta">
+                            Duplicar
+                          </button>
+                        </li>
+                      </ul>
+                  </div>
                 </div>
                 <div class="contenidoCard">
                 </div>
@@ -114,7 +125,7 @@ function crearTarjeta(button) {
       contenido.appendChild(card);
       inputName.value = "";
 
-      let nuevaTarea = new Tarea(name);
+      let nuevaTarea = new tarea(name);
       let jstring = JSON.stringify(nuevaTarea);
       localStorage.setItem(cardId, jstring);
       tarjId++;
@@ -122,9 +133,15 @@ function crearTarjeta(button) {
 }
 
 // para borrar columna
-function borrarColumna(button) {
-    let columna = button.closest(".contenedor");
-    columna.remove();
+function borrarColumna() {
+  if (columnaParaBorrarId) {
+    let column = document.getElementById(columnaParaBorrarId);
+    if (column) {
+        column.remove();
+        localStorage.removeItem(columnaParaBorrarId);
+        columnaParaBorrarId = null; // Reset the ID
+    }
+}
 }
 
 // borrar tarjeta
@@ -157,11 +174,11 @@ function cargarJson() {
           card.className = "card";
           card.id = cardId;
           card.innerHTML = `
-          <div class="card-body">
+          <div class="card-body" draggable = "">
           <div class="titulo">
-              <h4 class="${cardId}" style="color: black;">${taskData.nombre}</h4>
+              <h4 class="${cardId}" style="color: black;" contentEditable="true">${taskData.nombre}</h4>
               <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownTarjeta" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="btn btn-secondary" type="button" id="dropdownTarjeta" data-bs-toggle="dropdown" aria-expanded="false">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
                     <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                   </svg>
@@ -190,7 +207,7 @@ function cargarJson() {
       </div>
         `;
 
-          let contenido = contenedor.querySelector(".contenido");
+          let contenido = document.getElementById("contenido");
           contenido.appendChild(card);
         } catch (error) {
           console.error('Error parsing JSON:', error);
@@ -228,35 +245,6 @@ function contraerDescontraer(boton){
   }
 }
 
-function editarTarjeta(cardId) {
-  const card = document.getElementById(cardId);
-  const cardTitleElement = card.querySelector(".card-title");
-  
-  if (cardTitleElement) {
-      const cardTitle = cardTitleElement.textContent;
-
-      const modal = document.getElementById("editarTarjeta");
-      const modalCardTitle = modal.querySelector("#modalCardTitle");
-      modalCardTitle.value = cardTitle;
-      modal.dataset.cardId = cardId;
-  }
-}
-
-function guardarTarjetaEditada() {
-  let nombreEditado = document.getElementById("editarTarjeta").value;
-  let card = document.getElementById(cardToEditId);
-  if (card) {
-      const tituloElement = card.querySelector(".titulo h4");
-      if (tituloElement) {
-          tituloElement.textContent = nombreEditado;
-          // Update the card data in localStorage
-          const taskData = JSON.parse(localStorage.getItem(cardToEditId));
-          taskData.nombre = nombreEditado;
-          localStorage.setItem(cardToEditId, JSON.stringify(taskData));
-      }
-  }
-}
-
 
 function closeModal(modalId) {
   let modal = document.getElementById(modalId);
@@ -264,20 +252,15 @@ function closeModal(modalId) {
   bsModal.hide();
 }
 
-//nombre a editar
-let nameToEdit;
-function setNameToEdit(nombreEditar){
-  nameToEdit = nombreEditar;
-}
-
-function getNameToEdit(){
-  return nameToEdit;
-}
-
 // id de la tarjeta a borrarse
 let tarjetaParaBorrarId;
 function setIdToDeleteCard(cardId) {
   tarjetaParaBorrarId = cardId;
+}
+
+let columnaParaBorrarId;
+function setIdDeleteColumn(columnId){
+  columnaParaBorrarId = columnId;
 }
 
 let cardToEditId;
@@ -298,42 +281,6 @@ function botonCrearBoard(){
     alert(`Columna ${nombre} creada satisfactoriamente.`);
     craerBoard(nombre);
   }
-}
-
-// Crear Columna //
-function botonCrearColumna(){
-  const nombre = prompt("Ingrese el nombre de la columna")
-  if(nombre !== null && nombre.trim() !== ""){
-    alert(`Columna ${nombre} creada satisfactoriamente.`);
-    crearColumna(nombre);
-  }
-}
-
-// Crear Tarjeta //
-function botonCrearTarjeta(boton){
-  const name = prompt("Ingrese el nombre de la tarjeta");
-  if(name !== null && name.trim() !== ""){
-      alert(`Tarjeta ${name} creada satisfactoriamente.`);
-      crearTarjeta(boton, name);
-  }
-}
-
-// Borrar Columna //
-function alertaBorrarC(boton){
-  let texto;
-  if(confirm("¿Estas seguro que quieres eliminar el elemento?")){
-    texto = "Se eliminó el elemento."; 
-    borrarColumna(boton);
-    alert(texto);
-  } else {
-    texto = "No se eliminó ningun elemento.";
-    alert(texto);
-  }
-}
-
-// Borrar Tarjeta //
-function alertaBorrarT(boton){
-  borrarTarjeta(boton.parentNode.parentNode.parentNode);
 }
 
 // Comprimir y Descomprimir //
@@ -357,12 +304,28 @@ function drop(ev) {
   ev.target.appendChild(document.getElementById(data));
 }
 
+/*
+const container = document.getElementById('contenedor-principal');
+const existingButton = document.querySelector('.enter-button');
 
+// Agregar eventos al contenedor para delegación
+container.addEventListener('keydown', function(event) {
+    if (event.target.classList.contains('enter-as-button') && event.key === 'Enter') {
+        event.preventDefault(); // Evitar el salto de línea en el campo
+        event.target.nextElementSibling.click();
+    }
+});
 
-/* <=================================== Mains ===================================> */
+container.addEventListener('click', function(event) {
+    if (event.target.classList.contains('enter-button')) {
+        const buttonIndex = Array.from(event.target.parentElement.children).indexOf(event.target);
+        alert('Botón ' + (buttonIndex + 1) + ' clickeado');
+    }
+});
 
-crearColumna("Pendientes");
-crearColumna("En proceso");
-crearColumna("Terminadas");
+// Agregar evento al botón existente
+existingButton.addEventListener('click', function() {
+    alert('Botón Existente clickeado');
+});*/
 
 cargarJson();
